06a1ee010243001b20fd39650ff4ca2b15fe99df
Sagar Shelar <shaggy.shelar@gmail.com>
2017-08-23 16:09:47 +0530

    Added base projects and solutions

---
+++ CPQ.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CPQ.Web.Startup))]
+namespace CPQ.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
