e51e8476307689e8e8d08c58a2959cae5f2c159a
Sagar Shelar <shaggy.shelar@gmail.com>
2017-08-27 15:44:18 +0530

    All rules validation

--- CPQ.Web/Controllers/ProductsController.cs
+++ CPQ.Web/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CodeEffects.Rule.Common;
 using CodeEffects.Rule.Core;
 using CPQ.Domain;
 using CPQ.Persistance.Repositories;
@@ -23,20 +24,39 @@ namespace CPQ.Web.Controllers
         }
 
         [HttpGet]
-        public List<string> Evaluate(string id)
+        public Dictionary<string, List<string>> Evaluate(string id)
         {
             var products = _repo.All();
             var productViewModels = Mapper.Map<IEnumerable<ProductDto>>(products);
 
             List<string> results = new List<string>();
-            string ruleXml = StorageService.LoadRuleXml(id);
-            Evaluator<Product> evaluator = new Evaluator<Product>(ruleXml);
-            foreach (Product source in products)
+            Dictionary<string, List<string>> evalResults = new Dictionary<string, List<string>>();
+
+            var allRules = StorageService.GetEvaluationRules();
+            foreach (MenuItem rule in allRules)
             {
-                bool success = evaluator.Evaluate(source);
-                results.Add(string.Format("Product Name={0}, Result = {1}", source.Name, success));
+                string ruleXml = StorageService.LoadRuleXml(rule.ID);
+                Evaluator<Product> evaluator = new Evaluator<Product>(ruleXml);
+                foreach (Product source in products)
+                {
+                    bool success = evaluator.Evaluate(source);
+                    if (!success)
+                    {
+                        var errorMessage = "Failed: " + rule.ID + ", Name=" + source.Name;
+                        if (evalResults.ContainsKey(source.Id.ToString()))
+                        {
+                            evalResults[source.Id.ToString()].Add(errorMessage);
+                        }
+                        else
+                        {
+                            var errorsList = new List<string>() { errorMessage };
+                            evalResults.Add(source.Id.ToString(), errorsList);
+                        }
+                    }
+                    results.Add(string.Format("Product Name={0}, Result = {1}", source.Name, success));
+                }
             }
-            return results;
+            return evalResults;
         }
     }
 }
